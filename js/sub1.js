219edd81634bea367736209d1dfadbb01e0b72d3
Dongwook Lee <skyldw3@gmail.com>
2024-07-12 16:17:08 +0900

    first commit

--- js/sub1.js
+++ js/sub1.js
@@ -2,9 +2,13 @@ $(document).ready(function(){
     var a = 0,
         b = 0,
         c = 0,
-        d = 0;
-
-    $('.slider_img li').css({'opacity':'0.4'}).eq(0).css({'opacity':'0.1'})
+        d = 0,
+        $s_img = $('.slider_img li'),
+        $s_text = $('.slider_text li'),
+        e = 0,
+        f = 0;
+    
+    $('.slider_img li:first').css({'opacity':'0.4'})
     $('.slider_text li').css({'top':'6%', 'opacity':'0'})
         
     //---------------   헤더   ---------------
@@ -52,19 +56,38 @@ $(document).ready(function(){
     $('.slider_img li:first').animate({'opacity':'1'},1200)
     $('.slider_text li:first').animate({'top':'0', 'opacity':'1'},1600)
 
-    setInterval(function(){
-        $('.slider_img').animate({'left':'-100%'},1600,function(){
-            $('.slider_img li:first').appendTo('.slider_img')
-            $('.slider_img').css({'left':'0'})
+    function auto(){
+        $s_img.stop().fadeOut(1800,function(){
+            $(this).eq(e).appendTo('.slider_img')
         })
-        $('.slider_img li:first').stop().animate({'opacity':'0.4'},2000)
-        .siblings('li:first').stop().animate({'opacity':'1'},2000)
+        $s_text.stop().animate({'opacity':'0'},200,function(){
+            $s_text.not($s_text.eq(e)).css({'display':'block', 'top':'6%', 'opacity':'0'})
+        })
 
-        $('.slider_text li:first').stop().fadeOut(200).appendTo('.slider_text')
-        .siblings('li:first').stop().animate({'top':'0', 'opacity':'1'},1600,function(){
-            $('.slider_text li:gt(0)').css({'display':'block', 'top':'6%', 'opacity':'0'})
+        e=(e+1)%5
+        
+        $s_img.eq(e).stop().fadeIn(1800)
+        $s_text.eq(e).stop().animate({'top':'0', 'opacity':'1'},1800)
+    }
+
+    autoslide = setInterval(auto, 6000)
+
+    $('.btn').find('div').click(function(){
+        clearInterval(autoslide)
+        
+        $s_img.stop().fadeOut(600)
+        $s_text.stop().animate({'opacity':'0'},200,function(){
+            $s_text.not($s_text.eq(e)).css({'display':'block', 'top':'6%', 'opacity':'0'})
         })
-    },5000)
+        
+        f=$(this).index()
+        e=(e+4-(3*f))%5
+
+        $s_img.eq(e).stop().fadeIn(600)
+        $s_text.eq(e).stop().animate({'top':'0', 'opacity':'1'},800)
+
+        autoslide = setInterval(auto, 6000)
+    })
 
     $('.con1_main li').mouseover(function(){
         $(this).find('img').stop().animate({'opacity':'0.6'},80)
--- js/sub2.js
+++ js/sub2.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
     var a = 0,
         b = 0,
-        c = 0;
+        c = 0,
+        d = 0,
+        $h_con1_top = $('.con1_top_img').height(),
+        $h_con1_btm = $('.con1_btm_img1').height(),
+        $h_con2_top = $('.con2_top img').height(),
+        $h_con2_btm = $('.con2_btm img').height(),
+        $num = 0,
+        e = 0;
 
     //---------------   헤더   ---------------
     $('h1, .con1_main>img').css({'opacity':'0'})
@@ -13,6 +20,10 @@ $(document).ready(function(){
     .siblings('li').eq(2).delay(800).animate({'margin-top':'0', 'opacity':'1'},500)
     .siblings('li').eq(3).delay(900).animate({'margin-top':'0', 'opacity':'1'},500)
     .siblings('li').eq(4).delay(1000).animate({'margin-top':'0', 'opacity':'1'},500)
+
+    $('header, .h_logo').mouseover(function(){
+        $('header').addClass('blur')
+    })
 
     a=$(window).width()
     b=$('h1').height()
@@ -35,5 +46,83 @@ $(document).ready(function(){
     })
 
     //---------------   sub2 콘텐츠   ---------------
+    $('.slider h2:first').css({'opacity':'0', 'top':'24%'})
+    $('.slider h2:first').delay(800).animate({'opacity':'1', 'top':'14.8%'},function(){
+        $(this).animate({'top':'16.2%'},200,function(){
+            $(this).animate({'top':'16%'},200)
+        })
+    })
+    $('.slider ul').css({'opacity':'0'})
+    $('.slider h2:nth-child(2)').css({'top':'63%', 'opacity':'0'})
+    $('.slider ul').animate({'opacity':'1'},600)
+    $('.slider h2:nth-child(2)').delay(1600).animate({'top':'62%', 'opacity':'1'},800)
+
+    setInterval(function(){
+        $('.slider ul').stop().animate({'left':'-100%'},1000,function(){
+            $(this).find('li:first').appendTo(this)
+            $(this).css({'left':'0'})
+        })
+    },3000)
+
+    d=$('.carousel_group').width()
+    $('.text_carousel').animate({'left':-d},40000,'linear',function(){
+        $('.carousel_group:first').appendTo('.text_carousel')
+        $('.text_carousel').css({'left':'0'})
+    })
+
+    setInterval(function(){
+        d=$('.carousel_group').width()
+        $('.text_carousel').animate({'left':-d},40000,'linear',function(){
+            $('.carousel_group:first').appendTo('.text_carousel')
+            $('.text_carousel').css({'left':'0'})
+        })
+    },40000)
+
+    $('.con1_top_icon').animate({'rotate':'20deg'},1000,function(){
+        $('.con1_top_icon').animate({'rotate':'-5deg'},1000)
+    })
+
+    setInterval(function(){
+        $('.con1_top_icon').animate({'rotate':'20deg'},1000,function(){
+            $('.con1_top_icon').animate({'rotate':'-5deg'},1000)
+        })
+    },2000)
+
+    $('.con1_top').css({'height':$h_con1_top})
+    $('.con1_btm').css({'height':$h_con1_btm})
+    $('.con2_top .con2_slider ul').css({'height':$h_con2_top})
+    $('.con2_btm .con2_slider ul').css({'height':$h_con2_btm})
     
+    $h_con2_top=$h_con2_top+150
+    $h_con2_btm=$h_con2_btm+150
+
+    $('.con2_top').css({'height':$h_con2_top})
+    $('.con2_btm').css({'height':$h_con2_btm})
+
+    $('.btn').find('div').click(function(){
+        $(this).parent('.btn').parent('.con2_slider').find('li').stop().fadeOut(300)
+        
+        e=$(this).index()
+        $num=($num+((-2*e)+3))%4
+        
+        $(this).parent('.btn').parent('.con2_slider').find('li').eq($num).stop().fadeIn(300)
+    })
+
+    $(window).resize(function(){
+        $h_con1_top=$('.con1_top_img').height()
+        $h_con1_btm=$('.con1_btm_img1').height()
+        $h_con2_top=$('.con2_top img').height()
+        $h_con2_btm=$('.con2_btm img').height()
+        
+        $('.con1_top').css({'height':$h_con1_top})
+        $('.con1_btm').css({'height':$h_con1_btm})
+        $('.con2_top .con2_slider ul').css({'height':$h_con2_top})
+        $('.con2_btm .con2_slider ul').css({'height':$h_con2_btm})
+        
+        $h_con2_top=$h_con2_top+150
+        $h_con2_btm=$h_con2_btm+150
+
+        $('.con2_top').css({'height':$h_con2_top})
+        $('.con2_btm').css({'height':$h_con2_btm})
+    })
 })
