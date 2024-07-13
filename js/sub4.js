e15f7f63a4556b6b68ae405d7d573b57cd42e7d4
Dongwook Lee <skyldw3@gmail.com>
2024-07-14 02:33:40 +0900

    first commit

--- js/sub4.js
+++ js/sub4.js
@@ -35,5 +35,10 @@ $(document).ready(function(){
     })
 
     //---------------   sub4 콘텐츠   ---------------
-    
+    $('.main_title').animate({'top':'8%', 'opacity':'1'},1200,function(){
+        $('.main>p').animate({'opacity':'1'},600)
+        $('.main>a').delay(200).animate({'opacity':'1'},600)
+    })
+    $('.img_wrap img').delay(400).animate({'opacity':'1'},2000)
+
 })
